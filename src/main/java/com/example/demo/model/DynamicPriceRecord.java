e6195bb40ef684fe186970a3d836340142090a27
bala-amypo <bala@amypo.in>
2025-12-19 06:45:45 +0000

    User commit

--- src/main/java/com/example/demo/model/DynamicPriceRecord.java
+++ src/main/java/com/example/demo/model/DynamicPriceRecord.java
@@ -3,4 +3,12 @@ import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@R
+@Table(name="dynamic_price_record")
+public class DynamicPriceRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @C
+}
