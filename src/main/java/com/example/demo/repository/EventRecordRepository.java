b53bcaaa7a1d6bb6626bc38440a9c4c20935d787
bala-amypo <bala@amypo.in>
2025-12-20 17:30:10 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/EventRecordRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.EventRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface EventRecordRepository extends JpaRepository<EventRecord, Long> {
+    
+    boolean existsByEventCode(String code);
+    
+    Optional<EventRecord> findByEventCode(String code);
+}
